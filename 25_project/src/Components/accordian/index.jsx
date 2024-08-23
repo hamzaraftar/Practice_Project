4084e5b69cd66ef12097e85a29939e7896682462
Hamza Asghar <hamzaraftar765@gmail.com>
2024-08-23 23:07:08 +0500

    multiple  selection

--- 25_project/src/Components/accordian/index.jsx
+++ 25_project/src/Components/accordian/index.jsx
@@ -6,28 +6,49 @@ import "./style.css";
 
 const Accordian = () => {
   const [selected, setSelected] = useState(null);
+  const [enabelMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multiple, setMultiple] = useState([]);
 
   function handleSingleSelection(id) {
     setSelected(id === selected ? null : id);
   }
-  console.log(selected);
+
+  function handleMultiSelection(id) {
+    let cpyMultiple = [...multiple];
+    const findIndexOfCurrentId = cpyMultiple.indexOf(id);
+    console.log(findIndexOfCurrentId);
+    if (findIndexOfCurrentId === -1) cpyMultiple.push(id);
+    else cpyMultiple.splice(findIndexOfCurrentId, 1);
+    setMultiple(cpyMultiple);
+  }
 
   return (
     <div className="wrapper">
+      <button onClick={() => setEnableMultiSelection(!enabelMultiSelection)}>
+        Enable Multi Seleciton
+      </button>
       <div className="accordin">
         {data && data.length > 0 ? (
           data.map((items) => (
             <div className="items" key={data.id}>
               <div
-                onClick={() => handleSingleSelection(items.id)}
+                onClick={
+                  enabelMultiSelection
+                    ? () => handleMultiSelection(items.id)
+                    : () => handleSingleSelection(items.id)
+                }
                 className="title"
               >
                 <h3>{items.question}</h3>
                 <span>+</span>
               </div>
-              {selected === items.id && (
-                <div className="content">{items.answer}</div>
-              )}
+              {enabelMultiSelection
+                ? multiple.indexOf(items.id) !== -1 && (
+                    <div className="content">{items.answer}</div>
+                  )
+                : selected === items.id && (
+                    <div className="content">{items.answer}</div>
+                  )}
             </div>
           ))
         ) : (
