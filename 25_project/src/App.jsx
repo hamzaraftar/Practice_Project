620bf88b76e99534226b91bf7aa2762bd0412954
Hamza Asghar <hamzaraftar765@gmail.com>
2024-08-26 21:51:39 +0500

    Load more button

--- 25_project/src/App.jsx
+++ 25_project/src/App.jsx
@@ -1,14 +1,15 @@
 import "./App.css";
 import Accordian from "./Components/accordian";
+
 import RandomColor from "./Components/random-color";
 import StarRating from "./Components/start-rating";
 
 function App() {
   return (
     <>
-      {/* <Accordian /> */}
-      {/* <RandomColor /> */}
-      <StarRating noOFStarts={10} />
+      {/* <Accordian />
+      <RandomColor />
+      <StarRating noOFStarts={10} /> */}
     </>
   );
 }
---
+++ loan-more/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import LoadMoreData from "./Components";
+
+function App() {
+  return (
+    <>
+      <LoadMoreData />
+    </>
+  );
+}
+
+export default App;
