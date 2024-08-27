f1a68669cccb56b250d968e0027a222a2e49e602
Hamza Asghar <hamzaraftar765@gmail.com>
2024-08-27 19:53:30 +0500

    QR CODE GENERATOR

--- 25_project/src/App.jsx
+++ 25_project/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Accordian from "./Components/accordian";
+import QRCodeGenerator from "./Components/qr-code-generator";
 
 import RandomColor from "./Components/random-color";
 import StarRating from "./Components/start-rating";
@@ -10,6 +11,7 @@ function App() {
       {/* <Accordian />
       <RandomColor />
       <StarRating noOFStarts={10} /> */}
+      <QRCodeGenerator />
     </>
   );
 }
