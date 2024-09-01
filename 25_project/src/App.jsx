f3cd1b6bd28b2bf28bfdb204af84a17357bae45e
Hamza Asghar <hamzaraftar765@gmail.com>
2024-09-01 09:58:08 +0500

    scroll to top and bottom

--- 25_project/src/App.jsx
+++ 25_project/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import ScrollToTopAndBottem from "./Components/scroll-to-top-and-bottom";
 // import TicTacToe from "./Components/tic-tack-toe";
 // import GithubProfileFinder from "./Components/github-profilr-finder";
 // import ModelTest from "./Components/custom-model-popup/model-text";
@@ -10,7 +11,7 @@ import "./App.css";
 // import RandomColor from "./Components/random-color";
 // import ScrollIndecator from "./Components/scroll-indicator";
 // import StarRating from "./Components/start-rating";
-import UseFetchHooktest from "./Components/use-fetch";
+// import UseFetchHooktest from "./Components/use-fetch";
 
 function App() {
   return (
@@ -25,7 +26,8 @@ function App() {
       {/* <ModelTest /> */}
       {/* <GithubProfileFinder /> */}
       {/* <TicTacToe /> */}
-      <UseFetchHooktest />
+      {/* <UseFetchHooktest /> */}
+      <ScrollToTopAndBottem />
     </>
   );
 }
