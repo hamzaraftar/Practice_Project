5e68cbc908ee3d3dfc5bc7141118d8c7a2ce765f
Hamza Asghar <hamzaraftar765@gmail.com>
2024-10-21 21:16:43 +0500

    adding header

---
+++ move_app/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Menu from "./Menu";
+
+export default function Header() {
+  return (
+    <div>
+      <div className="">
+        <Menu title="home" address="/" Icone="" />
+      </div>
+      <div className=""></div>
+    </div>
+  );
+}
