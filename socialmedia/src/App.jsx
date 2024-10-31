7e3611badd77f9c264a2517caccc3eb062aa8285
Hamza Asghar <hamzaraftar765@gmail.com>
2024-10-31 08:26:24 -0700

    sidebar com

--- socialmedia/src/App.jsx
+++ socialmedia/src/App.jsx
@@ -2,13 +2,17 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Siderbar from "./components/Siderbar";
 
 function App() {
   return (
-    <>
+    <div className="app-container">
+      <Siderbar />
+      <div className="content">
       <Header />
       <Footer />
-    </>
+      </div>
+    </div>
   );
 }
 
