56a3b180376a17053728643b7f0eec5fdf9ffe95
Hamza Asghar <hamzaraftar765@gmail.com>
2024-10-31 08:32:39 -0700

    form com

--- socialmedia/src/App.jsx
+++ socialmedia/src/App.jsx
@@ -3,14 +3,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Siderbar from "./components/Siderbar";
+import CreatePost from "./components/CreatePost";
 
 function App() {
   return (
     <div className="app-container">
       <Siderbar />
       <div className="content">
-      <Header />
-      <Footer />
+        <Header />
+        <CreatePost />
+        <Footer />
       </div>
     </div>
   );
