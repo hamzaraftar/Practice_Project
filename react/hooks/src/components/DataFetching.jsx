236e7e0802ef709398a7f523eee721d0f4e0cac5
Hamza Asghar <hamzaraftar765@gmail.com>
2025-03-12 17:03:16 +0500

    data fetch using useReducer

---
+++ react/hooks/src/components/DataFetching.jsx
@@ -0,0 +1,30 @@
+import React, { useReducer, useEffect } from "react";
+import axios from "axios";
+
+const initialState = {
+  loading: true,
+  error: "",
+  post: {},
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_SUCCESS":
+      return { loading: false, post: action.payload, error: "" };
+
+    case "FETCH_ERROR":
+      return { loading: false, post: {}, error: "Something went wrong!" };
+
+    default:
+        return state;
+  }
+};
+
+export default function DataFetching() {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    useEffect( ()  => {
+        const res = axios
+    }, []);
+  return <div>DataFetching</div>;
+}
