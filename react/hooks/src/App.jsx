b57426e3eed275c4c46549e3fea511c9f368b54f
Hamza Asghar <hamzaraftar765@gmail.com>
2025-03-14 16:10:12 +0500

    custom hook

--- react/hooks/src/App.jsx
+++ react/hooks/src/App.jsx
@@ -2,14 +2,15 @@ import "./App.css";
 import State from "./components/_state";
 import Effect from "./components/Effect";
 import Reducer from "./components/Reducer";
-
+import DocTitle from "./components/DocTitle";
 
 function App() {
   return (
     <>
       {/* <State /> */}
       {/* <Effect /> */}
-      <Reducer />
+      {/* <Reducer /> */}
+      <DocTitle />
     </>
   );
 }
--- react/hooks/src/components/DataFetching.jsx
+++ react/hooks/src/components/DataFetching.jsx
@@ -1,30 +1,30 @@
-import React, { useReducer, useEffect } from "react";
-import axios from "axios";
+// import React, { useReducer, useEffect } from "react";
+// import axios from "axios";
 
-const initialState = {
-  loading: true,
-  error: "",
-  post: {},
-};
+// const initialState = {
+//   loading: true,
+//   error: "",
+//   post: {},
+// };
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "FETCH_SUCCESS":
-      return { loading: false, post: action.payload, error: "" };
+// const reducer = (state, action) => {
+//   switch (action.type) {
+//     case "FETCH_SUCCESS":
+//       return { loading: false, post: action.payload, error: "" };
 
-    case "FETCH_ERROR":
-      return { loading: false, post: {}, error: "Something went wrong!" };
+//     case "FETCH_ERROR":
+//       return { loading: false, post: {}, error: "Something went wrong!" };
 
-    default:
-        return state;
-  }
-};
+//     default:
+//         return state;
+//   }
+// };
 
-export default function DataFetching() {
-    const [state, dispatch] = useReducer(reducer, initialState);
+// export default function DataFetching() {
+//     const [state, dispatch] = useReducer(reducer, initialState);
 
-    useEffect( ()  => {
-        const res = axios
-    }, []);
-  return <div>DataFetching</div>;
-}
+//     useEffect( ()  => {
+//         const res = axios
+//     }, []);
+//   return <div>DataFetching</div>;
+// }
