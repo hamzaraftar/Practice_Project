275a3afe9cd9b23f41672d21f143c38bec20e4c7
Hamza Asghar <hamzaraftar765@gmail.com>
2025-11-05 17:54:47 +0500

    remove extra files

--- poll_project/frontend/src/Components/PermissionPopup.jsx
+++ poll_project/frontend/src/Components/PermissionPopup.jsx
@@ -13,22 +13,15 @@ export default function PermissionPopup({ message, onClose }) {
       </style>
 
       <div className="bg-white rounded-2xl shadow-2xl w-80 p-6 animate-fadeIn">
-        <div className="flex flex-col items-center text-center">
+        <div className="flex flex-col items-center text-center">        
           
-          {/* Icon */}
           <div className="w-14 h-14 rounded-full bg-red-100 flex items-center justify-center mb-3">
             <span className="text-red-600 text-3xl">!</span>
-          </div>
-
-          {/* Title */}
+          </div>          
           <h2 className="text-xl font-semibold text-gray-800 mb-2">
             Permission Required
-          </h2>
-
-          {/* Message */}
-          <p className="text-gray-600 mb-5 text-sm">{message}</p>
-
-          {/* Button */}
+          </h2>          
+          <p className="text-gray-600 mb-5 text-sm">{message}</p>          
           <button
             onClick={onClose}
             className="w-full py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 shadow-md transition cursor-pointer"
--- poll_project/frontend/src/Components/VoteChart.jsx
+++ poll_project/frontend/src/Components/VoteChart.jsx
@@ -1,5 +1,3 @@
-// src/VoteChart.js
-import React from "react";
 import { Bar } from "react-chartjs-2";
 import {
   Chart as ChartJS,
